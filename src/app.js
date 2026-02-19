3486511d67cde8ea232834b06ce5ea9a5171f9af
Mauricio Sabelli Machado <mnsabelli@gmail.com>
2026-02-19 12:50:18 +0000

    trabajo básico acabado

--- src/app.js
+++ src/app.js
@@ -5,23 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {};
+window.onload = function () {
 
+  const who = ['Mi vecino', 'Mi esposa', 'Mi cuñao', 'Mi mujer', 'Mi suegra'];
+  const action = ['tiró', 'pateó', 'deshizo', 'quemó'];
+  const what = ['mi ordenador', 'mi móvil', 'mi ropa', 'mis pantalones'];
+  const where = ['en el patio', 'en la cama', 'en la mesa', 'en el coche', 'en mi cara'];
 
-const who = ['Mi vecino', 'Mi esposa', 'Mi cuñao', 'Mi mujer', 'Mi suegra'];
-const action = ['tiró', 'pateó', 'deshizo', 'quemó'];
-const what = ['mi ordenador', 'mi móvil', 'mi ropa', 'mis pantalones'];
-const where = ['en el patio', 'en la cama', 'en la mesa', 'en el coche', 'en mi cara'];
+  const generadorDeNumerosRandom = (array) =>
+    Math.floor(Math.random() * array.length);
 
-const generadorDeNumerosRandom = (arraysConFrases) => Math.floor(Math.random()*arraysConFrases.length)
+  const entregaUnValorDelArray = (array) =>
+    array[generadorDeNumerosRandom(array)];
 
-const entregaUnValorDelArray = (arraysConFrases) => arraysConFrases[generadorDeNumerosRandom(arraysConFrases)]
+  function generarExcusa() {
+    const partesDeLaExcusa = [who, action, what, where];
 
-const sustantivos = [who,action,what,where];
+    const excuse = partesDeLaExcusa.reduce((acc, array) => {
+      return acc + entregaUnValorDelArray(array) + " ";
+    }, "");
 
-const excuse = sustantivos.reduce((acc, array)=>{
-    return acc + entregaUnValorDelArray(array) + " "
-}, " ");
+    document.getElementById("excuse").innerText = excuse;
+  }
 
-console.log(excuse);
+  // Generar una excusa al cargar la página
+  generarExcusa();
 
+  // Evento click del botón
+  document
+    .getElementById("btnExcusa")
+    .addEventListener("click", generarExcusa);
+};
