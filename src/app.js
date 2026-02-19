c52aa0207fce5f458e5cf2378f6b4f21e8a76ba3
Mauricio Sabelli Machado <mnsabelli@gmail.com>
2026-02-19 06:59:56 +0000

    generador funcionando

--- src/app.js
+++ src/app.js
@@ -5,16 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
+window.onload = function () {};
 
-const randomNumberGenerator = (arraysDelGenerador) => Math.floor(Math.random() * arraysDelGenerador.length)
-const randomPhraseFromArrays = (arraysDelGenerador) => arraysDelGenerador[randomNumberGenerator(arraysDelGenerador)]
 
 const who = ['Mi vecino', 'Mi esposa', 'Mi cuñao', 'Mi mujer', 'Mi suegra'];
 const action = ['tiró', 'pateó', 'deshizo', 'quemó'];
 const what = ['mi ordenador', 'mi móvil', 'mi ropa', 'mis pantalones'];
 const where = ['en el patio', 'en la cama', 'en la mesa', 'en el coche', 'en mi cara'];
 
-who.forEach((el, i)=> who[i])
+const generadorDeNumerosRandom = (arraysConFrases) => Math.floor(Math.random()*arraysConFrases.length)
 
-};
+const entregaUnValorDelArray = (arraysConFrases) => arraysConFrases[generadorDeNumerosRandom(arraysConFrases)]
+
+const sustantivos = [who,action,what,where];
+
+const excuse = sustantivos.reduce((acc, array)=>{
+    return acc + entregaUnValorDelArray(array) + " "
+}, " ");
+
+console.log(excuse);
+
